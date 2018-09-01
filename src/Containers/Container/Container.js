dde588f52ac5ac05919f0dc3a01b5f695d3ede37
Justine <justinecomment@gmail.com>
2018-09-01 19:38:38 +0200

    + integration fruit images

--- src/Containers/Container/Container.js
+++ src/Containers/Container/Container.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import './Container.css';
+import Pomme from '../../Assets/images/pomme.png';
+import Poire from '../../Assets/images/poire.png';
+import Fraise from '../../Assets/images/fraise.png';
+import Banane from '../../Assets/images/banane.png';
+import Kiwi from '../../Assets/images/kiwi.png';
+import Cerise from '../../Assets/images/cerise.png';
+import Ananas from '../../Assets/images/ananas.png';
+import Orange from '../../Assets/images/orange.png';
+import Pasteque from '../../Assets/images/pasteque.png';
+import Raisin from '../../Assets/images/raisin.png';
 
 class Container extends Component {
 
@@ -7,9 +17,10 @@ class Container extends Component {
     super(props);
     this.state = {
       liste: [
-        "1","2","3","4","5","6","7", "8", "9", "10"
+        Pomme,Poire,Fraise,Banane,Kiwi,Cerise, Ananas, Orange, Pasteque, Raisin 
       ],
       position : [],
+      radiusCircle : 330
     }
   }
 
@@ -53,31 +64,34 @@ class Container extends Component {
   drawCircle = () => {
     let nodeNumbers = this.state.liste.length;
     let element = this.state.liste;
-    let circleRadius = 300;
+    let circleRadius = this.state.radiusCircle;
     let circleWidth = circleRadius * 2 ; 
     let circleheight = circleRadius * 2 ; 
     let angle;
     let x;
     let y;
     let newPosition = [];
-
    
     for (let i=0 ; i < nodeNumbers ; i++){
       angle = (i / (nodeNumbers/2)) * Math.PI;
       x = (circleRadius * Math.cos(angle)) + (circleWidth / 2);
       y = (circleRadius * Math.sin(angle)) + (circleheight / 2);
       element = this.state.liste[i];
-      
       newPosition.push({element, x, y });
     }  
 
     this.setState({
-      position:newPosition
+      position: newPosition
     })
-
   }
 
-  render() {  
+  render() { 
+    
+    let images = this.state.liste.map((image, key) => {
+      return <img key={key} src={image} alt="image" />
+    })
+
+
     return (
       <div className="Container">
          <div className="button">
@@ -85,12 +99,13 @@ class Container extends Component {
           <button onClick={this.next}>next</button>
         </div> 
 
-
-        <svg height="600" width="620">
-          <circle cx="300" cy="300px" r="300" fill="#f7f7f7"/>
-         {this.state.position.map((item, key) => {
-          return <text key={key} x={item.x} y={item.y}>{item.element}</text>
-          })}
+        <svg height="600" width="600">
+          <circle cx="300" cy="500" r="400" fill="#fff"/>
+          <g className="firstCircle">
+            {this.state.position.map((item, key) => {
+              return <image xlinkHref={item.element} key={key} x={item.x} y={item.y} width="100" height="100" />
+              })}
+          </g>
         </svg>
       </div>
     );

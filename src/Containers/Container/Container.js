b0cdfbbf5e34527c0ec269e50f01d83a250e8cef
Justine <justinecomment@gmail.com>
2018-09-03 22:46:21 +0200

    + color center element of fruits's circle.

--- src/Containers/Container/Container.js
+++ src/Containers/Container/Container.js
@@ -1,41 +1,44 @@
 import React, { Component } from 'react';
 import './Container.css';
-import Pomme from '../../Assets/images/pomme.png';
-import Poire from '../../Assets/images/poire.png';
-import Fraise from '../../Assets/images/fraise.png';
-import Banane from '../../Assets/images/banane.png';
-import Kiwi from '../../Assets/images/kiwi.png';
-import Cerise from '../../Assets/images/cerise.png';
-import Ananas from '../../Assets/images/ananas.png';
-import Orange from '../../Assets/images/orange.png';
-import Pasteque from '../../Assets/images/pasteque.png';
-import Raisin from '../../Assets/images/raisin.png';
+import jsonsvg from '../../Assets/svgFruits.json';
 
 class Container extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      liste: [
-        Pomme,Poire,Fraise,Banane,Kiwi,Cerise, Ananas, Orange, Pasteque, Raisin 
-      ],
-      position : [],
-      radiusCircle : 330
+      liste: [],
+      position: [],
+      radiusCircle: 1000
     }
   }
 
-  componentDidMount(){
+  paths() {
+    let allPaths = [];
+    for (let svg in jsonsvg) {
+      let fruits = jsonsvg[svg].svgPath;
+      let d = [];
+      for (let index in fruits) {
+        let result = fruits[index];
+        d.push(result)
+      }
+      allPaths.push(d);
+    }
+    this.state.liste = allPaths;
+  }
+
+  componentDidMount() {
+    this.paths();
     this.drawCircle();
   }
 
-  update= (newListe) => {
+  update = (newListe) => {
     this.state.liste = newListe
     this.drawCircle();
   }
 
   next = () => {
     let newListe = [];
-    for(let i = 0; i < this.state.liste.length; i++){
+    for (let i = 0; i < this.state.liste.length; i++) {
       let newIndex = i + 1;
       if (newIndex > this.state.liste.length - 1) {
         newIndex = 0
@@ -43,18 +46,17 @@ class Container extends Component {
       newListe.push(this.state.liste[newIndex]);
     }
     this.setState({
-      liste : newListe
-    }) 
+      liste: newListe
+    })
     this.update(newListe);
-
   }
 
   previous = () => {
     let newListe = [];
-    for(let i = 0; i < this.state.liste.length; i++){
+    for (let i = 0; i < this.state.liste.length; i++) {
       let newIndex = i - 1;
       if (newIndex < 0) {
-        newIndex = this.state.liste.length -1
+        newIndex = this.state.liste.length - 1
       }
       newListe.push(this.state.liste[newIndex]);
     }
@@ -65,46 +67,69 @@ class Container extends Component {
     let nodeNumbers = this.state.liste.length;
     let element = this.state.liste;
     let circleRadius = this.state.radiusCircle;
-    let circleWidth = circleRadius * 2 ; 
-    let circleheight = circleRadius * 2 ; 
+    let circleWidth = circleRadius * 2;
+    let circleheight = circleRadius * 2;
     let angle;
     let x;
     let y;
     let newPosition = [];
-   
-    for (let i=0 ; i < nodeNumbers ; i++){
-      angle = (i / (nodeNumbers/2)) * Math.PI;
+
+    for (let i = 0; i < nodeNumbers; i++) {
+      angle = (i / (nodeNumbers / 2)) * Math.PI;
       x = (circleRadius * Math.cos(angle)) + (circleWidth / 2);
       y = (circleRadius * Math.sin(angle)) + (circleheight / 2);
+      let indexCenter = jsonsvg[jsonsvg.length -2].id;
+      let color = jsonsvg[i].color;     
       element = this.state.liste[i];
-      newPosition.push({element, x, y });
-    }  
-
+      
+      newPosition.push({ element, x, y, indexCenter, color});
+    }
     this.setState({
       position: newPosition
     })
   }
 
-  render() { 
-    
-    let images = this.state.liste.map((image, key) => {
-      return <img key={key} src={image} alt="image" />
-    })
-
-
+  render() {
     return (
       <div className="Container">
-         <div className="button">
-          <button onClick={this.previous}>previous</button>
-          <button onClick={this.next}>next</button>
-        </div> 
+        <div className="button">
+          <button onClick={this.previous}>
+            <i className="fas fa-caret-left"></i>
+          </button>
+          <button onClick={this.next}>
+            <i className="fas fa-caret-right"></i>
+          </button>
+        </div>
 
         <svg height="600" width="600">
-          <circle cx="300" cy="500" r="400" fill="#fff"/>
+          <circle cx="300" cy="500" r="400" fill="#fff" />
           <g className="firstCircle">
             {this.state.position.map((item, key) => {
-              return <image xlinkHref={item.element} key={key} x={item.x} y={item.y} width="100" height="100" />
-              })}
+              let x = item.x;
+              let y = item.y;
+              let color = "#D0D0D0";
+              let newColor = item.color;
+              console.log(item);
+              
+
+              if(key === item.indexCenter){
+                color = newColor;
+                console.log(item);
+              }
+
+              for (let elt in item) {
+                let pathComplet = [];
+                for(let index in item[elt]){
+                  let path = <path fill={color} d={item[elt][index].d} key={index} />
+                  pathComplet.push(path);
+                }
+                return (
+                  <g key={key} transform={`scale(0.3) translate(${x},${y})`}>
+                    {pathComplet}
+                  </g>
+                );
+              }
+            })}
           </g>
         </svg>
       </div>
